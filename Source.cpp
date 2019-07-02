a61d47c5c80dfa92f2649a9aea10f0e423eb7e9b
Gemiey <45759725+Gemiey@users.noreply.github.com>
2019-07-02 23:02:48 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -19,7 +19,10 @@ map<int, bool> checked;
 
 int v, m, d;
 
-void Translate(string original, string s) {
+void Translate(string original, string s) { 
+	// the function replaces all underscores with all possible combinations of 0s and 1s and stores them in the string variables s1 and s2. 
+	//The original variable, s is converted to its decimal equivalent. 
+	// Then, the newTable map is accessed, checked if it contains any data. The newTable map contains all binary string representations of each value.
 	std::size_t found = s.find('_');
 	if (found != std::string::npos) {
 		string s1 = s;
@@ -40,6 +43,8 @@ void Translate(string original, string s) {
 	}
 }
 void GetMinterms(string s) {
+	// The string has all underscores replaced with 0s and 1s to reach the original binary equivalent. It is then converted to decimal.
+	// The decimal value of the minterm is output.
 	std::size_t found = s.find('_');
 	if (found != std::string::npos) {
 		string s1 = s;
@@ -55,6 +60,8 @@ void GetMinterms(string s) {
 	}
 }
 void EPIcovered(string s) {
+	// The string s has all underscores replaced with 1s and 0s to reach the binary equivalent. Then it is converted to decimal.
+	// The vector checked marks its index as true, indicating it is a prime implicant.
 	std::size_t found = s.find('_');
 	if (found != std::string::npos) {
 		string s1 = s;
@@ -70,6 +77,11 @@ void EPIcovered(string s) {
 	}
 }
 void GetBinaryString(int n) {
+	// The integer n is converted to a 16 digit binary value stored in the string variable s.
+	// It is then reduced to a size of v, for the number of values in the program.
+	// The string is then checked for the number of 1s inside it.
+	// At the end of the function, s is stored in the matrix according to the number of 1s in its binary value.
+
 	string s = std::bitset<16>(n).to_string();//This value of 16 cannot be changed
 	s = s.substr(s.size() - v, s.size());
 	int counter = 0;
@@ -81,6 +93,8 @@ void GetBinaryString(int n) {
 	matrix[0][counter].push_back(s);
 }
 void Compare(int n, int m, string& s1, string& s2) {//n is the index of the column before the column of insertion and m is the number of ones
+	// The function compares the strings s1 and s2, and checks for any differences. The differences are replaced with an underscore.
+	// If there is only one difference and the underscore equivalent of the binary string does not already exist in the following column, it is placed in the column and the strings are marked with 'T'.
 	int difference = 0;
 	string temp = s1.substr(0, v);
 	for (int i = 0; i<v; i++) {
@@ -96,6 +110,7 @@ void Compare(int n, int m, string& s1, string& s2) {//n is the index of the colu
 	}
 }
 string ToBoolean(string s) {
+	// The string is converted to the boolean equivalent. This is achieved by using the equi array, which stores all possible letters.
 	string boolexp = "";
 	for (int i = 0; i<s.size(); i++) {
 		if (s[i] == '1') boolexp += equi[i];
@@ -121,10 +136,13 @@ int main() {
 	equi[14] = 'O';
 	equi[15] = 'P';
 	equi[16] = 'Q';
+
+	// file is opened and read from. 
+	
 	ifstream myfile("example.txt", ios::in);
 	string tempstring, tempstring1, tempstring2;
 	if (!myfile.is_open()) {
-		cout << "oepend" << endl;
+		cout << "File cannot open." << endl;
 	}
 	vector<string> str;
 	while (std::getline(myfile, tempstring)) {
@@ -157,6 +175,8 @@ int main() {
 
 
 	matrix.resize(v);
+
+	// validations are placed for the size of the minterms and don't cares and to avoid duplicates
 	for (int i = 0; i<v; i++) matrix[i].resize(v + 1);
 	for (int i = 0; i<m; i++) {
 		if (minterms[i]>pow(2, v) - 1) {
@@ -196,6 +216,9 @@ int main() {
 			}
 		}
 	}
+
+	// PIs are added
+
 	for (int g = 0; g<v; g++) {
 		for (int i = 0; i<v; i++) {
 			for (int j = 0; j<matrix[g][i].size(); j++) {
@@ -205,6 +228,9 @@ int main() {
 			}
 		}
 	}
+
+	// PIs are displayed in all formats
+
 	cout << "These are the prime implicants \n";
 	for (int i = 0; i<PI.size(); i++) {
 		Translate(PI[i], PI[i]);
@@ -218,11 +244,16 @@ int main() {
 		}
 	}
 
+	// EPIs are added and displayed.
+
 	cout << "These are the essential prime implicants\n";
 	for (int i = 0; i<EPI.size(); i++) {
 		cout << EPI[i] << " " << ToBoolean(EPI[i]) << endl;
 		EPIcovered(EPI[i]);
 	}
+
+	// Non essential PIs are displayed.
+
 	cout << "Those are the minterms that are not covered by essential prime implicants.\n";
 	for (int i = 0; i<m; i++) {
 		if (!checked[minterms[i]]) cout << minterms[i] << endl;
